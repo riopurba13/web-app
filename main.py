e96da70dd862e89fa1b47a31d353871f9d51965c
riopurba13 <rioadrianpurba59gmailcom@webmail.umm.ac.id>
2022-01-09 08:20:53 +0800

    debug port

--- main.py
+++ main.py
@@ -42,4 +42,6 @@ def send_uploaded_image(filename=''):
 
 
 if '__name__' == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 33507))
+    app.run(debug=True, host='0.0.0.0', port=port)
+    # app.run(debug=True)
